383d376758234541a81c45e927099a03b9e8f57b
yxy-start <15282716136@163.com>
2019-09-10 14:34:37 +0800

    模糊查询

--- js/index.js
+++ js/index.js
@@ -8,12 +8,12 @@ $(function(){
 		
 		$(".left-nav").on("click","li",function(event){
 			$(".left-nav > li").removeClass("current");
-			console.log(event.target);
+			// console.log(event.target);
 			var url = $(this).attr("url");
 			$(this).addClass("current");
 			// console.log($(this).attr("url"));
 			$("#wrapper").load(url+"?time",Math.random());
-
+			//+"?time",Math.random() 清除缓存
 		})
 		$("#first").trigger("click");
 		
